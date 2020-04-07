3b6f99cff6044c61b01fd1d6ee19a21432270778
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-04-06 17:46:10 -0700

    fix image

--- src/components/CartPod.js
+++ src/components/CartPod.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import Cart from './Cart';
+import cartpicture from './cart1.jpeg';
 
 const masterCartList = [
   {
-    name: 'cart 1',
-    menu: 'menu 1',
-    cartpic: img src=../images/'cart1.jpeg' alt='a picture'
+    name: "cart 1",
+    menu: "menu 1",
+    cartpic: <img src={cartpicture} height="42" width="42" alt="a picture" />,
   },
   {
-    name: 'cart 2',
-    menu: 'menu 2',
-    cartpic: 'cartpic 2'
+    name: "cart 2",
+    menu: "menu 2",
+    cartpic: "cartpic 2",
   },
   {
-    name: 'cart 3',
-    menu: 'menu 3',
-    cartpic: 'cartpic 3'
+    name: "cart 3",
+    menu: "menu 3",
+    cartpic: "cartpic 3",
   },
-
 ];
 
 const CartPod = () => {
