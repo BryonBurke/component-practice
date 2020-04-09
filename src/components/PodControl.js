00e5b0da8e5ce04303836bc352c3aa7d224bb192
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-04-09 11:53:50 -0700

    add page control logic

--- src/components/PodControl.js
+++ src/components/PodControl.js
@@ -9,16 +9,26 @@ class PodControl extends React.Component {
       formVisibleOnPage: false
      }
   }
+
+ handleClick = () =>{
+   this.setState(prevState => ({formVisibleOnPage: !prevState.formVisibleOnPage}));
+ }
+   
+
   render() { 
     let currentlyVisibleState = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage){
       currentlyVisibleState = <NewPodForm/>
+      buttonText = "Return to Cart Pod";
     } else {
       currentlyVisibleState = <CartPod/>
+      buttonText = "Add pod";
     }
     return ( 
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
      );
   }
