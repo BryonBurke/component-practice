8e058ed7a118e87e93b08fa8c1e4bf2968a6ac33
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-04-25 10:47:34 -0700

    add props to CartDetail.js

--- src/components/CartDetail.js
+++ src/components/CartDetail.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
-const CartlDetail = (props) => {
-  return ( 
+import PropTypes from "prop-types"
+const CartDetail = (props) => {
+  const { cart } = props;
+  return (
     <React.Fragment>
-      <h1>Cartl Detail</h1>
-      <hr/>
+      <h1>Cart Detail</h1>
+      <h3>{cart.name}</h3>
+      <h3>{cart.menu}</h3>
+      <h3>{cart.description}</h3>
+      <hr />
     </React.Fragment>
-   );
+  );
 }
  
-export default CartlDetail;
+CartDetail.propTypes = {
+  cart: PropTypes.object
+}
+
+export default CartDetail;
 
