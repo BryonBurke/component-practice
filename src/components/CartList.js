4f034e8c239ef1e02a49f86877a99012f01d8572
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-05-08 17:39:34 -0700

    add flex

--- src/components/CartList.js
+++ src/components/CartList.js
@@ -2,20 +2,56 @@ import React from 'react';
 import Cart from './Cart';
 import PropTypes from 'prop-types'
 
+const numItemsPerRow = 10;
+const spaceBetweenItems = 20; // edit to increase/decrease space between items
+
+const containerStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  margin: `-${spaceBetweenItems * 0.5}px`,
+  border: "10px solid black",
+  backgroundColor: "",
+};
+
+const itemStyle = {
+  display: "block",
+  flex: "none",
+  width: `${100 / numItemsPerRow}%`,
+  boxSizing: "border-box",
+  padding: `${spaceBetweenItems * 0.5}px`,
+  backgroundColor: "palevioletred",
+  textAlign: "center",
+  border: "10px solid blue",
+};
+
+const cardStyle = {
+  backgroundColor: "palevioletred",
+  height: "50px",
+  textAlign: "center",
+};
+
 const CartList = (props) => {
   return (
-    <React.Fragment>
-      <hr />
+    <React.Fragment
+    
+      style={containerStyle}>
+        <div style={containerStyle}>
       {props.cartList.map((cart) => (
-        <Cart
-          whenCartClicked = { props.onCartSelection }
-          name={cart.name}
-          menu={cart.menu}
-          description={cart.description}
-          id = {cart.id}
-          key = {cart.id}
-        />
+        <div style={itemStyle}>
+          <Cart
+            whenCartClicked = { props.onCartSelection }
+            name={cart.name}
+            menu={cart.menu}
+            description={cart.description}
+            id = {cart.id}
+            key = {cart.id}
+          />
+        </div>
       ))}
+
+
+        </div>
+   
     </React.Fragment>
   );
 }
